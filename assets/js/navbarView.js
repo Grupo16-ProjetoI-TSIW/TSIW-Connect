ab2d1a2b9d7e53daddbe32cf0450631b38193879
miguelfp17 <miguelfrancapereira@hotmail.com>
2024-06-20 16:59:54 +0100

    Registrar

--- assets/js/navbarView.js
+++ assets/js/navbarView.js
@@ -1,8 +1,25 @@
-// navbarView.js
-
+function RegisterPage(){
+    const loginModal = bootstrap.Modal.getInstance(document.getElementById('LoginModal'));
+    const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
+    document.getElementById('register-button').addEventListener('click', () => {
+        loginModal.hide();
+        registerModal.show();
+    });
+}
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('registerForm');
 
+    registerForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = document.getElementById('registerEmail').value;
+        const password = document.getElementById('registerPassword').value;
+        const nickname = document.getElementById('registerNickname').value;
+
+        
+        window.UserController.RegisterCredentials(email, password, nickname);
+    });
+    
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
